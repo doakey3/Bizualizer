e64f6516c01442416b4934b59ccd50a300d5888d
doakey3 <github.dmodo@spamgourmet.com>
2016-11-27 00:12:00 -0700

    install works on .zip level and importing song to vse adjusts timeline end

---
+++ __init__.py
@@ -0,0 +1,124 @@
+bl_info = {
+    "name": "Bizualizer",
+    "description": "Create a simple vizualizer for audio",
+    "author": "doakey3",
+    "version": (1, 1, 0),
+    "blender": (2, 7, 8),
+    "wiki_url": "https://github.com/doakey3/Bizualizer",
+    "tracker_url": "https://github.com/doakey3/Bizualizer/issues",
+    "category": "Animation",
+    "location": "Properties > Scene"}
+
+import bpy
+from .bizualizer import AudioToVSE, RemoveBZAudio, GenerateVizualizer
+
+
+class BizualizerUI(bpy.types.Panel):
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_label = "Bizualizer"
+    bl_context = "scene"
+
+    def draw(self, context):
+        layout = self.layout
+        scene = bpy.context.scene
+        row = layout.row()
+        row.prop(scene, "bz_audiofile", icon="SOUND")
+        row = layout.row()
+        row.prop(scene, "bz_audio_channel")
+        row = layout.row()
+        row.operator("sequencerextra.bz_audio_to_sequencer",
+                     icon="SEQ_SEQUENCER")
+        row.operator("sequencerextra.bz_audio_remove", icon="CANCEL")
+        row = layout.row()
+        row.prop(scene, "bz_bar_count")
+        row.prop(scene, "bz_bar_width")
+        row = layout.row()
+        row.prop(scene, "bz_amplitude")
+        row.prop(scene, "bz_spacing")
+        row = layout.row()
+        split = row.split()
+        col_a = split.column(align=True)
+        col_a.prop(scene, "bz_use_radial")
+        col_b = split.column(align=True)
+        col_b.prop(scene, "bz_radius")
+        if scene.bz_use_radial:
+            col_b.enabled = True
+        else:
+            col_b.enabled = False
+        row = layout.row()
+        row.operator("object.bz_generate", icon="RADIO")
+
+
+def initprop():
+    bpy.types.Scene.bz_audiofile = bpy.props.StringProperty(
+        name="Audio File",
+        description="Define path of the audio file",
+        subtype="FILE_PATH",
+        )
+
+    bpy.types.Scene.bz_audio_channel = bpy.props.IntProperty(
+        name="Audio Channel",
+        description="Channel where audio will be added",
+        default=1,
+        min=1
+        )
+
+    bpy.types.Scene.bz_bar_count = bpy.props.IntProperty(
+        name="Bar Count",
+        description="The number of bars to make",
+        default=64,
+        min=1
+        )
+
+    bpy.types.Scene.bz_bar_width = bpy.props.FloatProperty(
+        name="Bar Width",
+        description="The width of the bars",
+        default=0.8,
+        min=0
+        )
+
+    bpy.types.Scene.bz_amplitude = bpy.props.FloatProperty(
+        name="Amplitude",
+        description="Amplitude of visualizer bars",
+        default=24.0,
+        min=0
+        )
+
+    bpy.types.Scene.bz_use_radial = bpy.props.BoolProperty(
+        name="Use Radial",
+        description="Use a circular vizualizer",
+        default=False
+        )
+
+    bpy.types.Scene.bz_radius = bpy.props.FloatProperty(
+        name="Radius",
+        description="Radius of the radial vizualizer",
+        default=20,
+        min=0
+        )
+
+    bpy.types.Scene.bz_spacing = bpy.props.FloatProperty(
+        name="Spacing",
+        description="Spacing between bars",
+        default=2.25,
+        min=0
+        )
+
+
+def register():
+    bpy.utils.register_module(__name__)
+
+    initprop()
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+    del bpy.types.Scene.bz_audiofile
+    del bpy.types.Scene.bz_bar_count
+    del bpy.types.Scene.bz_bar_width
+    del bpy.types.Scene.bz_amplitude
+    del bpy.types.Scene.bz_spacing
+    del bpy.types.Scene.bz_use_radial
+    del bpy.types.Scene.bz_radius
--- bizualizer.py
+++ bizualizer.py
@@ -1,55 +1,7 @@
-bl_info = {
-    "name": "Bizualizer",
-    "description": "Create a simple vizualizer for audio",
-    "author": "doakey3",
-    "version": (1, 0, 4),
-    "blender": (2, 7, 8),
-    "wiki_url": "https://github.com/doakey3/Bizualizer",
-    "tracker_url": "https://github.com/doakey3/Bizualizer/issues",
-    "category": "Animation",
-    "location": "Properties > Scene"}
-
 import bpy
 import math
 import ntpath
 import sys
-
-
-class BizualizerUI(bpy.types.Panel):
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
-    bl_label = "Bizualizer"
-    bl_context = "scene"
-
-    def draw(self, context):
-        layout = self.layout
-        scene = bpy.context.scene
-        row = layout.row()
-        row.prop(scene, "bz_audiofile", icon="SOUND")
-        row = layout.row()
-        row.prop(scene, "bz_audio_channel")
-        row = layout.row()
-        row.operator("sequencerextra.bz_audio_to_sequencer",
-                     icon="SEQ_SEQUENCER")
-        row.operator("sequencerextra.bz_audio_remove", icon="CANCEL")
-        row = layout.row()
-        row.prop(scene, "bz_bar_count")
-        row.prop(scene, "bz_bar_width")
-        row = layout.row()
-        row.prop(scene, "bz_amplitude")
-        row.prop(scene, "bz_spacing")
-        row = layout.row()
-        split = row.split()
-        col_a = split.column(align=True)
-        col_a.prop(scene, "bz_use_radial")
-        col_b = split.column(align=True)
-        col_b.prop(scene, "bz_radius")
-        if scene.bz_use_radial:
-            col_b.enabled = True
-        else:
-            col_b.enabled = False
-        row = layout.row()
-        row.operator("object.bz_generate", icon="RADIO")
 
 
 class AudioToVSE(bpy.types.Operator):
@@ -76,6 +28,25 @@ class AudioToVSE(bpy.types.Operator):
 
         sound_strip = scene.sequence_editor.sequences.new_sound(
             "bz_" + name, audiofile, chan, start)
+
+        
+
+        frame_start = 300000
+        frame_end = -300000
+        for strip in scene.sequence_editor.sequences:
+            try:
+                if strip.frame_final_start < frame_start:
+                    frame_start = strip.frame_final_start
+                if strip.frame_final_end > frame_end:
+                    frame_end = strip.frame_final_end - 1
+            except AttributeError:
+                pass
+
+        if frame_start != 300000:
+            scene.frame_start = frame_start
+        if frame_end != -300000:
+            scene.frame_end = frame_end
+
         return {"FINISHED"}
 
 
@@ -215,83 +186,3 @@ def update_progress(job_title, progress):
         msg += " DONE\r\n"
     sys.stdout.write(msg)
     sys.stdout.flush()
-
-
-def initprop():
-    bpy.types.Scene.bz_audiofile = bpy.props.StringProperty(
-        name="Audio File",
-        description="Define path of the audio file",
-        subtype="FILE_PATH",
-        )
-
-    bpy.types.Scene.bz_audio_channel = bpy.props.IntProperty(
-        name="Audio Channel",
-        description="Channel where audio will be added",
-        default=1,
-        min=1
-        )
-
-    bpy.types.Scene.bz_bar_count = bpy.props.IntProperty(
-        name="Bar Count",
-        description="The number of bars to make",
-        default=64,
-        min=1
-        )
-
-    bpy.types.Scene.bz_bar_width = bpy.props.FloatProperty(
-        name="Bar Width",
-        description="The width of the bars",
-        default=0.8,
-        min=0
-        )
-
-    bpy.types.Scene.bz_amplitude = bpy.props.FloatProperty(
-        name="Amplitude",
-        description="Amplitude of visualizer bars",
-        default=24.0,
-        min=0
-        )
-
-    bpy.types.Scene.bz_use_radial = bpy.props.BoolProperty(
-        name="Use Radial",
-        description="Use a circular vizualizer",
-        default=False
-        )
-
-    bpy.types.Scene.bz_radius = bpy.props.FloatProperty(
-        name="Radius",
-        description="Radius of the radial vizualizer",
-        default=20,
-        min=0
-        )
-
-    bpy.types.Scene.bz_spacing = bpy.props.FloatProperty(
-        name="Spacing",
-        description="Spacing between bars",
-        default=2.25,
-        min=0
-        )
-
-
-def register():
-    bpy.utils.register_class(BizualizerUI)
-    bpy.utils.register_class(AudioToVSE)
-    bpy.utils.register_class(GenerateVizualizer)
-    bpy.utils.register_class(RemoveBZAudio)
-
-    initprop()
-
-
-def unregister():
-    bpy.utils.unregister_class(BizualizerUI)
-    bpy.utils.unregister_class(AudioToVSE)
-    bpy.utils.unregister_class(RemoveBZAudio)
-    bpy.utils.unregister_class(GenerateVizualizer)
-
-    del bpy.types.Scene.bz_audiofile
-    del bpy.types.Scene.bz_bar_count
-    del bpy.types.Scene.bz_bar_width
-    del bpy.types.Scene.bz_amplitude
-    del bpy.types.Scene.bz_spacing
-    del bpy.types.Scene.bz_use_radial
-    del bpy.types.Scene.bz_radius
